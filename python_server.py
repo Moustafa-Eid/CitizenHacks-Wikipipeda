e0d2996e188914db0fcf90101fe723c96335fef3
Luis Zugasti <luis.zugasti@outlook.com>
2019-09-07 14:42:01 -0400

    add requirements.txt file

--- python_server.py
+++ python_server.py
@@ -2,19 +2,26 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
+
+## Subroutine for webpages
 # Proven that it works for facebook (in current state)
-url = "https://www.facebook.com/legal/terms/plain_text_terms"
+def webpage(url):
+    url = "https://www.facebook.com/legal/terms/plain_text_terms"
 
-r = requests.get(url)
-# Parse through the text to find "terms of usage"
-location = [m.start() for m in re.finditer('terms of service', r.text.lower())]
-soup = r.text[location[0]:]
+    r = requests.get(url)
+    # Parse through the text to find "terms of usage"
+    location = [m.start() for m in re.finditer('terms of service', r.text.lower())]
+    soup = r.text[location[0]:]
 
-location = [m.start() for m in re.finditer('<script>', soup.lower())]
+    location = [m.start() for m in re.finditer('<script>', soup.lower())]
 
-soup_stripped = soup[:location[0]]
-soup_stripped = BeautifulSoup(soup_stripped, "html.parser").get_text()
+    soup_stripped = soup[:location[0]]
+    soup_stripped = BeautifulSoup(soup_stripped, "html.parser").get_text()
 
-# Prints our desired piece of code!!!
-print(soup_stripped)
-# All text.
+    # Prints our desired piece of code!!!
+    print(soup_stripped)
+    # All text.
+    return soup_stripped
+
+def pdf(file_location)
+
