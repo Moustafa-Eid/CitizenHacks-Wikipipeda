9d967f8a68acb6209be530074938730cc5008b5e
Luis Zugasti <luis.zugasti@outlook.com>
2019-09-07 14:32:11 -0400

    corrects the behavior for facebook webpage

--- python_server.py
+++ python_server.py
@@ -2,23 +2,19 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-url = "https://www.facebook.com/legal/terms/update"
+# Proven that it works for facebook (in current state)
+url = "https://www.facebook.com/legal/terms/plain_text_terms"
+
 r = requests.get(url)
+# Parse through the text to find "terms of usage"
+location = [m.start() for m in re.finditer('terms of service', r.text.lower())]
+soup = r.text[location[0]:]
 
-print(r.text + "\n")
+location = [m.start() for m in re.finditer('<script>', soup.lower())]
 
-# remove the header information
-if "facebook" in url:
-    print(r.text[19])
-    tester = [m.start() for m in re.finditer('terms of service', r.text.lower())]
-    print(tester)
-    # in steps: remove everything above the first time "terms of service" is mentioned
-    tester1 = r.text[tester[0]:]
-    print(tester1)
-    # remove everything beyond the first "script" call
-    tester = [m.start() for m in re.finditer('<script', tester1.lower())]
-    tester1 = tester1[:tester[0]]
-    print(tester1)
-    # remove all tags, just keep the text
-    sanitized = BeautifulSoup(tester1, "html.parser")
-    print(sanitized.text)
+soup_stripped = soup[:location[0]]
+soup_stripped = BeautifulSoup(soup_stripped, "html.parser").get_text()
+
+# Prints our desired piece of code!!!
+print(soup_stripped)
+# All text.
